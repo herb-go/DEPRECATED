c87c71ee4b4e94ff53e82180d424f98c8733d770
jarlyyn <jarlyyn@qq.com>
2020-08-24 23:03:12 +0800

    update

---
+++ memberform/msg.go
@@ -0,0 +1,9 @@
+package memberform
+
+import "github.com/herb-go/herb/ui"
+
+//MsgIncorrectUsernameOrPassword message that shows incorrect username or password.
+var MsgIncorrectUsernameOrPassword = ui.NewMessage("herbgo.member", "Incorrect username or password.")
+
+//MsgUserNotAvailable message that shows field incorrect
+var MsgUserNotAvailable = ui.NewMessage("herbgo.member", "User is currently not available.")
